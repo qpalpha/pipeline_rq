dca01d36965c81203725a8bbd3aebd1d31eec722
qpalpha <qpalpha@163.com>
2019-08-16 18:31:43 +0800

    lx: bug

--- job.daily.close.sh
+++ job.daily.close.sh
@@ -4,7 +4,6 @@ echo "-------------- (`date`) Hfdata Job after Close Start --------------"
 GIT_PATH=/home/qp/git/pipeline_rq
 
 cd $GIT_PATH
-python3 job.tick.daily.py
-python3 job.mb1.daily.py
+python3 job.daily.close.py
 
 echo "-------------- (`date`) Hfdata Job after Close End --------------"
