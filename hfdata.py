38207dec0906b02d7a81d468e11f06590653ede9
qpalpha <qpalpha@163.com>
2019-08-19 09:25:50 +0800

    lx: bug

--- hfdata.py
+++ hfdata.py
@@ -138,7 +138,7 @@ class Snapshot():
         mat = np.hstack([mat,dvec]).astype(float)
         tickers = [tickers_dict[data._order_book_id] for data in data_list]
         # Return
-        self.catch_result = mat,tickers,dtime
+        self.catch_result = mat,tickers,data_list[0].datetime.strftime('%Y%m%d.%H%M%S')
         return mat,tickers,dtime
 
     def save(self,file:str=None):
