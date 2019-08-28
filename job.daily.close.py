f846aa0271a738cc4a60b0367d4c62a7380a72fc
qpalpha <qpalpha@163.com>
2019-08-28 10:31:07 +0800

    lx: job.daily.close.py

--- job.daily.close.py
+++ job.daily.close.py
@@ -19,12 +19,13 @@ if __name__=='__main__':
     # tick
     tick = TickData('./ini/tick.history.ini')
     sdate = date_offset(edate,-5)
-    tick.get_raw_csv(sdate,edate)
-    # mb1
-    tick = TickData('./ini/mb1.history.ini')
-    sdate = date_offset(edate,-3)
-    tick.tick2mb1(sdate,edate)
-    # mb1->mb5/15/30
-    for minute in ['5','15','30']:
-        mb = MBData('./ini/mb.ini',minute)
-        mb.to_csv(sdate,edate)
+    for type in ['CS','ETF','INDX','Future','Option']:
+        tick.get_raw_csv(sdate,edate,type=type)
+    ## mb1
+    #tick = TickData('./ini/mb1.history.ini')
+    #sdate = date_offset(edate,-3)
+    #tick.tick2mb1(sdate,edate)
+    ## mb1->mb5/15/30
+    #for minute in ['5','15','30']:
+    #    mb = MBData('./ini/mb.ini',minute)
+    #    mb.to_csv(sdate,edate)
